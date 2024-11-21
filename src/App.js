e46fe4667c10704af127d35ef39da63a5a4dce5e
Paulo251 <paulorico1100@gmail.com>
2024-11-21 17:41:55 -0300

    adicionar base react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'semantic-ui-css/semantic.min.css'
+
+
+function App() {  
+  return(
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App;
